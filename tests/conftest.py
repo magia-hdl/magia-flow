b3d227d23d793d46a6c58fee4ce09a3b4cc1a551
khwong-c64 <kin.hin.wong.c@gmail.com>
2024-03-02 14:27:55 +0000

    Testing package

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,3 @@
-from pathlib import Path
-from tempfile import TemporaryDirectory
-
-import pytest
-
-from .helper import init_verilator
+from magia_flow.simulation.verilator import init_verilator
 
 init_verilator()
-
-
-@pytest.fixture()
-def temp_build_dir():
-    with TemporaryDirectory() as tmpdir:
-        yield str(Path(tmpdir).absolute())
